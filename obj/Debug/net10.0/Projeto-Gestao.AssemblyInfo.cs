078370feefe783bd99da2f21d98613850d21cede
Tiago Alves <tiagoanascimento17@hotmail.com>
2026-04-08 22:30:27 -0300

    edit view dash e estoque

--- obj/Debug/net10.0/Projeto-Gestao.AssemblyInfo.cs
+++ obj/Debug/net10.0/Projeto-Gestao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c376fff97a0a6c5ccaca435fd5255f6aa52b8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00dfb9a9c40bdad7221ca0329e90d4370ebc4e0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
