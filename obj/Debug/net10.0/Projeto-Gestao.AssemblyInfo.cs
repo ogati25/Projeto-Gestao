8a98b4368bd8a5a55ffb5a9d009bb4e1b1f3e89b
Tiago Alves <tiagoanascimento17@hotmail.com>
2026-05-15 22:17:40 -0300

    campo chip adicionado novamente

--- obj/Debug/net10.0/Projeto-Gestao.AssemblyInfo.cs
+++ obj/Debug/net10.0/Projeto-Gestao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac09793f8f740903818332b251e03699c248362a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73344cd5503b8e42105ae6d52a5a2f3baa1fee4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
