0009c144cec8805b132705a8f0b7e68bb4a37244
Tiago Alves <tiagoanascimento17@hotmail.com>
2026-06-06 14:17:50 -0300

    edit files

--- obj/Debug/net10.0/Projeto-Gestao.AssemblyInfo.cs
+++ obj/Debug/net10.0/Projeto-Gestao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8844d8ee17daf7879cfa7ac281feea80dbbffda9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3af1d4cebc7bb23874d6a51ff805e0594d2307c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
